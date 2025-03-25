ce0665b98bbfd4a8013b107db60f8ab8a2696783
alyapr <putririzaldyalya@gmail.com>
2025-03-25 12:52:56 +0700

    firebase

---
+++ daftar_belanja/lib/screens/shopping_list_screen.dart
@@ -0,0 +1,7 @@
+import 'package:daftar_belanja/services/shopping_service.dart';
+import 'package:flutter/widgets.dart';
+
+StatefulWidget ShoppingListScreen{
+  final TextEditingController _controller = TextEditingController();
+  final ShoppingService _shoppingService = ShoppingService();
+}
